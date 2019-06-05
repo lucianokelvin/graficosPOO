972cdab01917e07a3debaf8fd7759d0d418be5e1
Gabriely Pereira <gabriely.pereira@usp.br>
2019-06-05 10:23:04 -0300

    Changing api.js file to use a CDN
    The code from colors.js was moved to api.js to be used at https://cdn.jsdelivr.net/gh/lucianokelvin/graficosPOO@master/api/api.js

--- api/api.js
+++ api/api.js
@@ -1,4 +1,25 @@
-// var url = 'http://localhost:3000';
+/*!
+ * MsFLOSS JS Library
+ * (c) 2019 MsFLOSS Data Visualization Contributors
+ */
+
+window.chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    lightBlue : 'rgb(204, 204, 255)'
+
+ }
+
+window.colors = [];
+window.colors.push(window.chartColors.red);
+window.colors.push(window.chartColors.orange);
+window.colors.push(window.chartColors.yellow);
+
+
 var url = 'https://msfloss.herokuapp.com';
 
 async function numberOfCommits(elementId){
